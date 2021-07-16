8e14cc044fdc277625316fd580235c8a0d8e8087
Elison Liu <elisonliu97@gmail.com>
2021-07-16 18:10:59 -0400

    fetch portion of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,6 +10,8 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// GOTTEN MAINLY FROM FOLDER 12-INS-CACHING-FETCHING-FILES
+
 // INSTALLATION EVENT LISTENER
 self.addEventListener("install", function (event) {
     event.waitUntil(
@@ -36,4 +38,38 @@ self.addEventListener("activate", function (event) {
         })
     );
     self.clients.claim();
+});
+
+// FETCH EVENT LISTENER
+self.addEventListener("fetch", function (event) {
+    // cache successful requests to the API
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
