ffe8e117c36e6d1d97267bfebb3acf62f532ea67
Elison Liu <elisonliu97@gmail.com>
2021-07-16 18:13:10 -0400

    look for service worker in index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js").then(reg => {
+      console.log("We found your service worker file!", reg);
+    });
+  });
+}
+
 let transactions = [];
 let myChart;
 
