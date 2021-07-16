b9aa932c652247b8972ed267684325da1f756beb
Elison Liu <elisonliu97@gmail.com>
2021-07-16 18:02:19 -0400

    cache portion of service worker

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "index.js",
+    "/db.js",
+    "public/styles.css",
+    "/manifest.webmanifest",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
